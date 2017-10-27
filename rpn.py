4c6b359d07b78494baf5ae1557fa2298dd5aac9b
sltries <sltries@umich.edu>
2017-10-27 12:36:07 -0400

    added mult and div

--- rpn.py
+++ rpn.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python3
+import operator
+def add(a, b)
+    return a + b
+
+
+
+ops = {
+    '+' : operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv,
+}
 
 def calculate(myarg):
     stack = list()
     for token in myarg.split():
-        if token == '+':
-            arg2 = stack.pop()
-            arg1 = stack.pop()
-            result = arg1 + arg2
-            stack.append(result)
-        elif token == '-':
-            arg2 = stack.pop()
-            arg1 = stack.pop()
-            result = arg1 - arg2
-            stack.append(result)
-        else:
+        try:
             stack.append(int(token))
+        except ValueError:
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            function = ops[token]
+            result = function(arg1, arg2)
+            stack.append(result) 
     print(stack)
     return stack.pop()
 
