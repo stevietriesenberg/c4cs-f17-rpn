df9268a87f329828708a7b09e81e33d16552880f
sltries <sltries@umich.edu>
2017-11-07 23:52:45 -0500

    lots of stuff

--- rpn.py
+++ rpn.py
@@ -1,4 +1,4 @@
-#!/mport operator
+import operator
 
 
 operators = {
@@ -32,40 +32,4 @@ def main():
 
 if __name__ == '__main__':
     main()
-usr/bin/env python3
-import operator
-def add(a, b)
-    return a + b
-
-
-
-ops = {
-    '+' : operator.add,
-    '-': operator.sub,
-    '*': operator.mul,
-    '/': operator.truediv,
-}
-
-def calculate(myarg):
-    stack = list()
-    for token in myarg.split():
-        try:
-            stack.append(int(token))
-        except ValueError:
-            arg2 = stack.pop()
-            arg1 = stack.pop()
-            function = ops[token]
-            result = function(arg1, arg2)
-            stack.append(result) 
-    print(stack)
-    return stack.pop()
-
-def main():
-    while True:
-        calculate(input("rpn calc> "))
-
-if __name__ == '__main__':
-    main()
-
-
 
--- test_rpn.py
+++ test_rpn.py
@@ -1,4 +1,4 @@
-mport unittest
+import unittest
 
 import rpn
 
@@ -15,13 +15,4 @@ class TestBasics(unittest.TestCase):
     def test_divide(self):
         result = rpn.calculate("6 3 /")
         self.assertEqual(2, result)
-import unittest
-import rpn
 
-class TestBasics(unittest.TestCase):
-    def test_add(self):
-        result = rpn.calculate('1 1 +')
-        self.assertEqual(2, result)
-    def test_subtract(self):
-        result = rpn.calculate('11 2 -')
-        self.assertEqual(9, result)
